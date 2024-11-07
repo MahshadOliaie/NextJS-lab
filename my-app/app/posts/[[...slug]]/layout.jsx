cd8909be02c3bc2387ae5d01e9c3b6e99937a693
MahshadOliaie <mahshadoliaie@gmail.com>
2024-11-07 18:51:36 +0330

    nested layout

---
+++ my-app/app/posts/[[...slug]]/layout.jsx
@@ -0,0 +1,9 @@
+
+export default function PostLayout({ children }) {
+    return (
+        <>
+            <h1 style={{ backgroundColor: "lightblue" }}>posts layout</h1>
+            {children}
+        </>
+    )
+}
